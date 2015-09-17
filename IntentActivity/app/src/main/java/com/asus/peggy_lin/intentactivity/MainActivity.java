2c22a5bde2a55e1b3d7a2ae25c7ba9ca927fed72
loofah666 <bluepeg66@gmail.com>
2015-09-17 14:51:36 +0800

    add goToCamera add EmailIntentActivity

--- IntentActivity/app/src/main/java/com/asus/peggy_lin/intentactivity/MainActivity.java
+++ IntentActivity/app/src/main/java/com/asus/peggy_lin/intentactivity/MainActivity.java
@@ -1,16 +1,40 @@
 package com.asus.peggy_lin.intentactivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
+    private static final int CAMERA_CODE = 360;
+    private ImageView cameraImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, CAMERA_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == CAMERA_CODE){
+            if(resultCode == RESULT_OK){
+                Uri uri = data.getData();
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                if(bmp!=null)
+                    cameraImage.setImageBitmap(bmp);
+            }
+        }
     }
 
     @Override
