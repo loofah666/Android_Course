f96a15427f4e0adbdfcab74a33fd0d62aa48a28c
loofah666 <bluepeg66@gmail.com>
2015-09-21 14:39:37 +0800

    api min 8

--- IntentActivity/app/src/main/java/com/asus/peggy_lin/intentactivity/MainActivity.java
+++ IntentActivity/app/src/main/java/com/asus/peggy_lin/intentactivity/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Build;
 import android.provider.MediaStore;
 //import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
