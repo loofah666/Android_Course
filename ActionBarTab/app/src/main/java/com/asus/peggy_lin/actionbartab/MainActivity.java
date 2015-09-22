973c694de657d58d596b5e26b2a55266e269c620
loofah666 <bluepeg66@gmail.com>
2015-09-22 17:32:31 +0800

    follow Android example with 3 swiping tabs

--- ActionBarTab/app/src/main/java/com/asus/peggy_lin/actionbartab/MainActivity.java
+++ ActionBarTab/app/src/main/java/com/asus/peggy_lin/actionbartab/MainActivity.java
@@ -1,17 +1,126 @@
 package com.asus.peggy_lin.actionbartab;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity  extends FragmentActivity implements ActionBar.TabListener {
+
+    AppSectionsAdapter mSectionsAdapter;
+    ViewPager mViewPager;
+    ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActionBar = getActionBar();
+        //don't enable UP Nav
+        mActionBar.setHomeButtonEnabled(false);
+        //Displays Tabs
+        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // adapter that returns fragments
+        mSectionsAdapter = new AppSectionsAdapter(getSupportFragmentManager());
+
+        //ViewPager with PageListener
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                //make tab selected
+                mActionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        //Add Tabs
+        for (int i = 0; i < mSectionsAdapter.getCount(); i++) {
+            mActionBar.addTab(
+                    mActionBar.newTab()
+                            .setText(mSectionsAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // select tab
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    public  class AppSectionsAdapter extends FragmentPagerAdapter {
+        public AppSectionsAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+//                case 0:
+
+                default:
+                    // The other sections of the app are dummy placeholders.
+                    Fragment fragment = new DummySectionFragment();
+                    Bundle args = new Bundle();
+                    args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
+                    fragment.setArguments(args);
+                    return fragment;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public String getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return getString(R.string.section_1);
+                case 1:
+                    return getString(R.string.section_2);
+                case 2:
+                    return getString(R.string.section_3);
+                default:
+                    return getString(R.string.section_other);
+            }
+        }
+    }
+
+    public static class DummySectionFragment extends Fragment {
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_section_dummy, container, false);
+            Bundle args = getArguments();
+            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+                    getString(R.string.dummy_section_text, args.getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
     }
 
     @Override
@@ -30,10 +139,8 @@ public class MainActivity extends Activity {
 
         switch(id) {
             case R.id.action_settings:
-                Toast.makeText(getApplicationContext(), R.string.action_settings, Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_search:
-                Toast.makeText(getApplicationContext(), R.string.action_search, Toast.LENGTH_SHORT).show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
