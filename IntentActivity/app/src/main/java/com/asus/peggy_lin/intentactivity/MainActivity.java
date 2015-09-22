ff867bbd952d03a88114a3c6822651ee80ce3c1e
loofah666 <bluepeg66@gmail.com>
2015-09-22 10:39:36 +0800

    update

--- IntentActivity/app/src/main/java/com/asus/peggy_lin/intentactivity/MainActivity.java
+++ IntentActivity/app/src/main/java/com/asus/peggy_lin/intentactivity/MainActivity.java
@@ -7,14 +7,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.provider.MediaStore;
-//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -83,21 +80,9 @@ public class MainActivity extends Activity {
     //avatar click enables photo switch
     public void switchProfilePhoto(View view) {
         final String[] arr = getResources().getStringArray(R.array.profile_array);
-//        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_view);
-//            arrayAdapter.addAll(arr);
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.create();
-//        builder.setTitle(R.string.profile_action);
-//        builder.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int which) {
-//                // The 'which' argument contains the index position of the selected item
-//                if (which == 0)
-//                    goToCamera();
-//                else
-//                    Toast.makeText(getApplicationContext(), arr[which], Toast.LENGTH_SHORT).show();
-//            }
-//        });
         builder.setTitle(R.string.profile_action)
                 .setItems(arr, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
@@ -109,14 +94,15 @@ public class MainActivity extends Activity {
             }
         });
         builder.show();
-        //return builder.create();
     }
 
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
