3af92e15b03c9def37a0df6bf652fe6c364a07eb
loofah666 <bluepeg66@gmail.com>
2015-10-08 17:06:02 +0800

    added showExternal with listView of DCIM/Camera

--- SavingData/app/src/main/java/com/asus/peggy_lin/savingdata/MainActivity.java
+++ SavingData/app/src/main/java/com/asus/peggy_lin/savingdata/MainActivity.java
@@ -2,8 +2,10 @@ package com.asus.peggy_lin.savingdata;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
@@ -21,10 +23,8 @@ public class MainActivity extends Activity {
     TextView tv_def, tv_show;
     EditText et_user, et_write;
     String filename = "userFile";
-    String fileData = "";
-    String userBlog = "";
+    String fileData, userBlog, sharedPreferenceName = "";
     LinearLayout layout_new, layout_old, layout_write, layout_show;
-    String sharedPreferenceName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +35,36 @@ public class MainActivity extends Activity {
         //Context context = this;
 
         //shared preferences when multi files
-        //SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
         //preference when only one activity uses
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        //SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
 
+        //if string not found then defaultValue
         String defaultValue = "";
         sharedPreferenceName = sharedPref.getString(getString(R.string.saved_name_str), defaultValue);
 
+        //init views
         tv_def = (TextView) findViewById(R.id.save_data_text_view);
-
         layout_new = (LinearLayout) findViewById(R.id.layout_new_user);
         layout_old = (LinearLayout) findViewById(R.id.layout_old_user);
         layout_write = (LinearLayout) findViewById(R.id.layout_blog_write);
         layout_show = (LinearLayout) findViewById(R.id.layout_blog_show);
 
+
         if(sharedPreferenceName != "") {
+            //not first time using
             oldUser(sharedPreferenceName);
 
             File file = new File(getFilesDir(), filename);
             if(file.exists())
+                //if intro text is saved before
                 readFile();
             else if(!file.exists())
+                //if intro text is not found
                 editFile();
         }else
+            //first time using
             newUser();
     }
 
@@ -67,23 +73,30 @@ public class MainActivity extends Activity {
         String newNameStr = et_user.getText().toString();
 
 //        Context context = this;
-//        SharedPreferences sharedPref = getSharedPreferences(
-//                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
 
+        //write into sharedPreferences
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString(getString(R.string.saved_name_str), newNameStr);
         editor.commit();
 
+        //switch to oldUser mode and editFile as default
         oldUser(newNameStr);
         editFile();
     }
 
     public void deleteSharedPreference(View view){
+        //delete intro text that goes with user
         delTextFile(view);
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        //delete sharedPreferences
+//        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         sharedPref.edit().remove(getString(R.string.saved_name_str)).commit();
 
+        //switch to newUser mode
         newUser();
     }
 
@@ -102,6 +115,7 @@ public class MainActivity extends Activity {
         layout_write.setVisibility(View.GONE);
         layout_show.setVisibility(View.VISIBLE);
 
+        //reading from file that's found
         StringBuilder sb = new StringBuilder();
         try{
             FileInputStream inputStream = openFileInput(filename);
@@ -115,8 +129,10 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
 
+        //show file content
         tv_show = (TextView) findViewById(R.id.save_file_text_view);
         tv_show.setText(sb.toString());
+
     }
 
     public void editFile(){
@@ -125,11 +141,11 @@ public class MainActivity extends Activity {
     }
 
     public void saveTextFile(View view){
-        FileOutputStream outputStream;
-
         et_write = (EditText) findViewById(R.id.save_file_edit_text);
         userBlog = et_write.getText().toString();
 
+        //write into internal storage
+        FileOutputStream outputStream;
         try {
             outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
             outputStream.write(userBlog.getBytes());
@@ -137,11 +153,24 @@ public class MainActivity extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //switch to readFile mode
         readFile();
     }
 
     public void delTextFile(View view){
+        //delete intro text
         new File(getFilesDir(), filename).delete();
+        //switch to editFile mode
         editFile();
     }
+
+    public void goToShowExternal(View view){
+        //go to External Storage Practice
+        Intent intent = new Intent(this, ShowExternal.class);
+        startActivity(intent);
+    }
+
+
+
 }
---
+++ SavingData/app/src/main/java/com/asus/peggy_lin/savingdata/ShowExternal.java
@@ -0,0 +1,75 @@
+package com.asus.peggy_lin.savingdata;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+
+public class ShowExternal extends Activity {
+
+    String LOG_EXT = "EXTERNAL_STORAGE";
+    String LOG_FILES = "EXTERNAL_FILES";
+    ListView lv;
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_external);
+
+        lv = (ListView) findViewById(R.id.external_list_view);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        lv.setAdapter(adapter);
+
+        if(isExternalStorageWritable()) {
+            Log.d(LOG_EXT, "is Writable & Readable");
+            getAlbumStorageDir();
+
+        }
+        else if(isExternalStorageReadable())
+            Log.d(LOG_EXT, "is Readable");
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void getAlbumStorageDir() {
+        //get DCIM/Camera directory path
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/Camera").toString();
+        File file = new File(path);
+
+        //if directory not exists
+        if (!file.mkdirs()) {
+            //create directory
+            file.mkdir();
+        }
+
+        //list array of files in directory
+        File file_list[] = file.listFiles();
+
+        for (int i = 0; i < file_list.length; i++) {
+            adapter.add(file_list[i].getName());
+            Log.d(LOG_FILES, "FileName:" + file_list[i].getName());
+        }
+    }
+}
