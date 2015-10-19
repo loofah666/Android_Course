fb5b109ab12e6606b180c92f42245f862acf22e3
peggy_lin <peggy_lin@asus.com>
2015-10-19 19:23:28 +0800

    intent of email, contact, calendar, text SMS

--- IntentPractice/app/src/main/java/com/asus/peggy_lin/intentpractice/MainActivity.java
+++ IntentPractice/app/src/main/java/com/asus/peggy_lin/intentpractice/MainActivity.java
@@ -1,16 +1,31 @@
 package com.asus.peggy_lin.intentpractice;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
+
+    private static final String TAG_INTENT = "INTENT_PRAC";
+    public static final int PICK_CONTACT_REQUEST = 1;  // The request code
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                goToEmail();
             }
         });
     }
@@ -32,7 +47,108 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         if(toolbar != null){
             setSupportActionBar(toolbar);
-            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setTitle(getString(R.string.group_title));
+    }
+
+    public void goToNewPost(){
+
+    }
+
+    public void goToEmail(){
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        // The intent does not have a URI, so declare the "text/plain" MIME type
+        emailIntent.setType("text/plain");
+        String[] mail = {getString(R.string.email_address)};
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, mail);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+        emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text));
+        //emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
+
+        String title = getResources().getString(R.string.chooser_email);
+        Intent chooser = Intent.createChooser(emailIntent, title);
+
+        // Verify it resolves
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(chooser, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        // Start an activity if it's safe
+        if (isIntentSafe) {
+            startActivity(chooser);
+        }
+    }
+
+    public void goToCalendar(View view){
+//        Log.d(TAG_INTENT, "calendar");
+        Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(year, 11, 31, 8, 30);  //month is 0~11, 11=December
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(year, 11, 31, 18, 30);
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.Events.TITLE, "Adoption Center Year Annual Event");
+        calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Adoption Center");
+
+        // Verify it resolves
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(calendarIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        // Start an activity if it's safe
+        if (isIntentSafe) {
+            startActivity(calendarIntent);
+        }
+    }
+
+    public void goToContacts(){
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    public void goToTextMessaging(String number){
+        Intent textIntent = new Intent(Intent.ACTION_SENDTO);
+        textIntent.setType("text/plain");
+        textIntent.setData(Uri.parse("sms:" + number));
+        textIntent.putExtra("sms_body", getString(R.string.text_text));
+//        textIntent.putExtra(Intent.EXTRA_STREAM, attachment);
+
+        // Verify it resolves
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(textIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        // Start an activity if it's safe
+        if (isIntentSafe) {
+            startActivity(textIntent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_CONTACT_REQUEST){
+            if(resultCode == RESULT_OK){
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                Log.d(TAG_INTENT, "number: " + number);
+                goToTextMessaging(number);
+            }
         }
     }
 
@@ -50,10 +166,17 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_email:
+                goToNewPost();
+                return true;
+            case R.id.action_send:
+                goToContacts();
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
